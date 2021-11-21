1b4b4769263a94c707a54be2359072554751e79c
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-21 13:32:24 -0500

    Add files via upload

---
+++ ET3/Pregunta2/Nodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta2
+{
+    internal class Nodo
+    {
+        public int valor;
+        public string valor2;
+        public Nodo izquierdo;
+        public Nodo derecho;
+    }
+}
